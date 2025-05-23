e08538885053d151d0ce869219f09b08f0061dcc
angel-nc11 <anajarro572@gmail.com>
2025-05-22 22:40:22 -0600

    conection to database

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 <p align="center">
-  <img src="https://www.mysql.com/common/logos/logo-mysql-170x115.png" alt="MySQL" width="120"/>
-  <img src="https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg" alt="Java" width="80"/>
+    <img src="https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg" alt="Java" width="50"/>
+    <img src="https://www.mysql.com/common/logos/logo-mysql-170x115.png" alt="MySQL" width="100"/>
 </p>
 
+java v21
+mysql v8
+
 # ACADEMY PROJECT
+Aplicacion de escritorio para una academia de cursos libres
---
+++ academia/src/main/java/org/academy/Model/JDBCUtil.java
@@ -0,0 +1,31 @@
+package org.academy.Model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JDBCUtil {
+    private static final Properties props = new Properties();
+    static {
+        try(InputStream in = JDBCUtil.class.getClassLoader()
+                .getResourceAsStream("db.Properties")){
+                if (in==null){
+                    throw new RuntimeException("no se encuentra el archivo");
+                }
+                props.load(in);
+        } catch (IOException e) {
+            throw new ExceptionInInitializerError("Error al cargar el archivo: "+ e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException{
+        String url = props.getProperty("jdbc.url");
+        String user = props.getProperty("jdbc.user");
+        String password = props.getProperty("jdbc.password");
+        return DriverManager.getConnection(url,user,password);
+    }
+
+}
